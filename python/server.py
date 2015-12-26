5a338b6b935b43ffcbc9a5cedf1f54e6b98a3e5b
Tom Parker <palfrey@tevp.net>
2015-12-26 18:15:46 +0000

    Make server.py use tabs, and initial client adding

--- python/server.py
+++ python/server.py
@@ -5,83 +5,101 @@ import zmq
 import time
 import threading
 import json
-from voluptuous import Schema, Required, All, Length, MultipleInvalid, Extra
+from voluptuous import Schema, Required, All, Length, MultipleInvalid, Extra, Range
 import uuid
 
 def UUID(value):
-    return uuid.UUID(value)
+	return uuid.UUID(value)
 
 class Client:
-    def __init__(self):
-        self.zmq = None
+	def __init__(self, zmq):
+		self.zmq = zmq
 
 class JSONResource:
-    def check_req(self, req):
-        try:
-            data = json.loads(req.stream.read().decode("utf-8"))
-            if type(data) != dict:
-                raise falcon.HTTPBadRequest("Invalid JSON", "Request was not a JSON dictionary")
-            StoreResource.schema(data)
-            return data
-        except ValueError:
-            raise falcon.HTTPBadRequest("Invalid JSON", "Request was not valid JSON")
-        except MultipleInvalid as e:
-            raise falcon.HTTPInvalidParam("Invalid keys", e.msg)
+	def check_req(self, req):
+		try:
+			data = json.loads(req.stream.read().decode("utf-8"))
+			if type(data) != dict:
+				raise falcon.HTTPBadRequest("Invalid JSON", "Request was not a JSON dictionary")
+			StoreResource.schema(data)
+			return data
+		except ValueError:
+			raise falcon.HTTPBadRequest("Invalid JSON", "Request was not valid JSON")
+		except MultipleInvalid as e:
+			raise falcon.HTTPInvalidParam("Invalid keys", e.msg)
 
 class ClientResource(JSONResource):
-    def __init__(self):
-        self.clients = []
+	def __init__(self, context, poller):
+		self.clients = []
+		self.context = context
+		self.poller = poller
 
-    def on_get(self, req, resp):
-        if len(req.stream.read()) > 0:
-            raise falcon.HTTPBadRequest("clients accepts no arguments", "Gave a body to a method that doesn't accept one")
-        return self.clients
+	def on_get(self, req, resp):
+		if len(req.stream.read()) > 0:
+			raise falcon.HTTPBadRequest("clients accepts no arguments", "Gave a body to a method that doesn't accept one")
+		return self.clients
+
+	schema = Schema({
+		Required("host"): All(str, Length(min=1)),
+		Required("port"): All(int, Range(min=1))
+	})
+
+	def on_put(self, req, resp):
+		data = self.check_req(req)
+		zmq = self.context.socket(zmq.SUB)
+		zmq.connect("tcp://{host}:{port}".format(**data))
+		self.poller.append(zmq)
+		self.clients.append(Client(zmq))
 
 class StoreResource(JSONResource):
-    def __init__(self, db):
-        self.db = db
+	def __init__(self, db):
+		self.db = db
 
-    schema = Schema({
-        Required("table"): All(str, Length(min=1)),
-        Required("id"): UUID,
-        Required("entry_id"): UUID,
-        Required("data"): {Extra: object}
-    })
+	schema = Schema({
+		Required("table"): All(str, Length(min=1)),
+		Required("id"): UUID,
+		Required("entry_id"): UUID,
+		Required("data"): {Extra: object}
+	})
 
-    def on_put(self, req, resp):
-        data = self.check_req(req)
-        key = data["entry_id"].encode("utf-8")
-        try:
-            existing = self.db.Get(key)
-            raise falcon.HTTPConflict("Duplicate key", "Already have entry for %s" % key)
-        except KeyError:
-            self.db.Put(key, json.dumps(data["data"]).encode("utf-8"))
+	def on_put(self, req, resp):
+		data = self.check_req(req)
+		key = data["entry_id"].encode("utf-8")
+		try:
+			existing = self.db.Get(key)
+			raise falcon.HTTPConflict("Duplicate key", "Already have entry for %s" % key)
+		except KeyError:
+			self.db.Put(key, json.dumps(data["data"]).encode("utf-8"))
 
-def ZMQ(poller):
+def ZMQ(poller, event):
 	while True:
+		if event.is_set():
+			break
 		if len(poller.sockets) == 0:
-			time.sleep(5)
+			event.wait(timeout = 5)
 			continue
 		try:
 			socks = dict(poller.poll())
 		except KeyboardInterrupt:
 			break
+		except zmq.error.ContextTerminated:
+			break
 		print ("socks", socks)
 
 def make_api(db_path = "./db", port = "5555"):
-    api = falcon.API()
-    db = leveldb.LevelDB(db_path, paranoid_checks = True)
-    api.add_route('/clients', ClientResource())
-    api.add_route('/store', StoreResource(db))
-    context = zmq.Context.instance()
-    clients = context.socket(zmq.ROUTER)
-    clients.bind("tcp://*:" + str(port))
-    poller = zmq.Poller()
-    poller.register(clients, zmq.POLLIN)
-
-    thread = threading.Thread(target=ZMQ, args=(poller, ), daemon = True)
-    thread.start()
-    return api
+	api = falcon.API()
+	db = leveldb.LevelDB(db_path, paranoid_checks = True)
+	context = zmq.Context.instance()
+	poller = zmq.Poller()
+	api.add_route('/clients', ClientResource(context, poller))
+	api.add_route('/store', StoreResource(db))
+	clients = context.socket(zmq.ROUTER)
+	clients.bind("tcp://*:" + str(port))
+	poller.register(clients, zmq.POLLIN)
+	event = threading.Event()
+	thread = threading.Thread(target=ZMQ, args=(poller, event), daemon = True)
+	thread.start()
+	return {"api": api, "context": context, "thread": thread, "event": event}
 
 if __name__ == "__main__":
-    api = make_api()
+	api = make_api()["api"]
--- python/test/server_test.py
+++ python/test/server_test.py
@@ -9,8 +9,8 @@ import uuid
 
 JSONEncoder_olddefault = json.JSONEncoder.default
 def JSONEncoder_newdefault(self, o):
-    if isinstance(o, uuid.UUID): return str(o)
-    return JSONEncoder_olddefault(self, o)
+	if isinstance(o, uuid.UUID): return str(o)
+	return JSONEncoder_olddefault(self, o)
 json.JSONEncoder.default = JSONEncoder_newdefault
 
 json_st = st.recursive(st.floats() | st.booleans() | st.text() | st.none(), lambda children: st.lists(children) | st.dictionaries(st.text(), children))
@@ -31,8 +31,12 @@ class ServerTest(testing.TestBase):
 				continue
 			self.ports.append(port)
 			break
-		self.api = server.make_api(tempfile.mkdtemp(), port)
+		self.info = server.make_api(tempfile.mkdtemp(), port)
+		self.api = self.info["api"]
 		self.existing_stores = []
+
+	def after(self):
+		self.info["event"].set()
 
 	@given(st.text(min_size = 1))
 	def test_clients_dislikes_args(self, s):
