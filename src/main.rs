681414e4167746de9e392b5d2c72bebd13990b98
Tom Parker <palfrey@tevp.net>
2016-05-23 23:35:40 +0100

    Very initial web request handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
 #[macro_use] extern crate schemamama;
 extern crate schemamama_postgres;
 extern crate postgres;
+mod schema;
+
+#[macro_use] extern crate log;
+extern crate log4rs;
+
+extern crate pencil;
+use pencil::{Pencil, Request, Response, PencilResult};
+//use pencil::http_errors::BadRequest;
 
 use std::env;
 
-mod schema;
+fn log_status(request: &mut Request) -> PencilResult {
+    info!("{:?}", request);
+    Ok(Response::from("Hello World!"))
+}
 
 fn main() {
-    let db_url: &str = &env::var("DATABASE_URL").unwrap();
-    let conn = postgres::Connection::connect(db_url, postgres::SslMode::None).unwrap();
-    schema::up(&conn);
+    log4rs::init_file("log.yaml", Default::default()).unwrap();
+    let db_url: &str = &env::var("DATABASE_URL").expect("Needed DATABASE_URL");
+    let conn = postgres::Connection::connect(db_url, postgres::SslMode::None).expect("Needed a working DATABASE_URL");
+    schema::up(&conn).unwrap();
+    let mut app = Pencil::new("");
+    app.set_debug(true);
+    app.set_log_level();
+    app.get("/log", "log status", log_status);
+    app.post("/log", "add new local log entry", log_status);
+    app.get("/log/<entry_id:string>", "get log with UUID", log_status);
+    app.run("127.0.0.1:8000");
 }
--- src/schema.rs
+++ src/schema.rs
@@ -9,7 +9,7 @@ migration!(CreateLog, 201605221254, "create log table");
 
 impl PostgresMigration for CreateLog {
     fn up(&self, transaction: &postgres::Transaction) -> Result<(), postgres::error::Error> {
-        transaction.execute("CREATE TABLE log (id UUID PRIMARY KEY, next UUID, prev UUID, data JSON);", &[]).unwrap();
+        transaction.execute("CREATE TABLE log (id UUID PRIMARY KEY, owner UUID, next UUID, prev UUID, data JSON);", &[]).unwrap();
         return Ok(());
     }
 
