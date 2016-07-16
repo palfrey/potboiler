fcd121b43260c31be799929be650d7955378f834
Tom Parker <palfrey@tevp.net>
2016-07-16 23:49:29 +0100

    Log all requests

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,30 @@ extern crate log4rs;
 
 extern crate iron;
 extern crate router;
+extern crate logger;
 
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use logger::Logger;
 
 use std::env;
 
-fn log_status(request: &mut Request) -> IronResult<Response> {
-    info!("{:?}", request);
+fn log_status(_: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "Hello World!")))
+}
+
+fn new_log(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "New log")))
+}
+
+fn get_log(req: &mut Request) -> IronResult<Response> {
+    let ref query = req.extensions
+        .get::<Router>()
+        .unwrap()
+        .find("entry_id")
+        .unwrap_or("/");
+    Ok(Response::with((status::Ok, format!("Get log {}", query))))
 }
 
 fn main() {
@@ -28,9 +42,13 @@ fn main() {
     let conn = postgres::Connection::connect(db_url, postgres::SslMode::None)
         .expect("Needed a working DATABASE_URL");
     schema::up(&conn).unwrap();
+    let (logger_before, logger_after) = Logger::new(None);
     let mut router = Router::new();
     router.get("/log", log_status);
-    router.post("/log", log_status);
-    router.get("/log/:entry_id", log_status);
-    Iron::new(router).http("localhost:8000").unwrap();
+    router.post("/log", new_log);
+    router.get("/log/:entry_id", get_log);
+    let mut chain = Chain::new(router);
+    chain.link_before(logger_before);
+    chain.link_after(logger_after);
+    Iron::new(chain).http("localhost:8000").unwrap();
 }
