86c109816b34a7872e9fdf1786b2a776e2f8c07d
Tom Parker <palfrey@tevp.net>
2016-10-25 23:05:02 +0100

    Check for both postgres and core for kv

---
+++ wait-for-port.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+host="${1:-postgres}"
+port="${2:-5432}"
+
+while true; do
+  # bash specific!
+  echo > "/dev/tcp/$host/$port"
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  >&2 echo "Port $port is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Port $port is up"
