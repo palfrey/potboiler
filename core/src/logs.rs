49960c7c0852a947f842317a8b15e9fdaae5fc15
Tom Parker <palfrey@tevp.net>
2016-10-30 14:14:14 +0000

    Split out logs code from main

---
+++ core/src/logs.rs
@@ -0,0 +1,136 @@
+use clock;
+use hybrid_clocks;
+use iron;
+use iron::modifiers::Redirect;
+use iron::prelude::{Response, IronResult, Request, IronError};
+use iron::status;
+use nodes;
+use notifications;
+use persistent;
+use postgres::rows::{Row, RowIndex};
+use postgres::types::FromSql;
+use potboiler_common::{db, server_id};
+use router::Router;
+use serde_json::{self, Value, Map};
+use serde_types::Log;
+use std::io::{Cursor, Read};
+use std::ops::Deref;
+use std::sync::Arc;
+use uuid::Uuid;
+
+fn log_status<T: Into<String>>(req: &mut Request, stmt: T) -> IronResult<Response> {
+    let conn = get_pg_connection!(&req);
+    let stmt = conn.prepare(&stmt.into()).expect("prepare failure");
+    let mut logs = Map::new();
+    for row in &stmt.query(&[]).expect("last select works") {
+        let id: Uuid = row.get("id");
+        let owner: Uuid = row.get("owner");
+        logs.insert(owner.to_string(), serde_json::to_value(&id.to_string()));
+    }
+    let value = Value::Object(logs);
+    Ok(Response::with((status::Ok, serde_json::ser::to_string(&value).unwrap())))
+}
+
+pub fn log_lasts(req: &mut Request) -> IronResult<Response> {
+    log_status(req, "SELECT id, owner from log WHERE next is null")
+}
+
+pub fn log_firsts(req: &mut Request) -> IronResult<Response> {
+    log_status(req, "SELECT id, owner from log WHERE prev is null")
+}
+
+pub fn new_log(mut req: &mut Request) -> IronResult<Response> {
+    let conn = get_pg_connection!(&req);
+    let body_string = {
+        let mut body = String::new();
+        req.body.read_to_string(&mut body).expect("could read from body");
+        body
+    };
+    let json: Value = match serde_json::de::from_str(&body_string) {
+        Ok(val) => val,
+        Err(err) => return Err(IronError::new(err, (status::BadRequest, "Bad JSON"))),
+    };
+    let id = Uuid::new_v4();
+    let hyphenated = id.hyphenated().to_string();
+    let server_id = get_server_id!(&req).deref().clone();
+    let stmt = conn.prepare("SELECT id from log WHERE next is null and owner = $1 LIMIT 1")
+        .expect("prepare failure");
+    let results = stmt.query(&[&server_id]).expect("last select works");
+    let previous = if results.is_empty() {
+        None
+    } else {
+        let row = results.get(0);
+        let id: Uuid = row.get("id");
+        Some(id)
+    };
+    let when = clock::get_timestamp(&mut req);
+    let log = Log {
+        id: id,
+        owner: server_id.clone(),
+        prev: previous,
+        next: None,
+        when: when,
+        data: json.clone(),
+    };
+    nodes::insert_log(&conn, &log);
+    let log_arc = Arc::new(log);
+    notifications::notify_everyone(req, log_arc.clone());
+    nodes::notify_everyone(req, log_arc.clone());
+    let new_url = {
+        let req_url = req.url.clone();
+        let base_url = req_url.into_generic_url();
+        base_url.join(&format!("/log/{}", &hyphenated)).expect("join url works")
+    };
+    Ok(Response::with((status::Created,
+                       Redirect(iron::Url::from_generic_url(new_url).expect("URL parsed ok")))))
+}
+
+fn get_with_null<I, T>(row: &Row, index: I) -> Option<T>
+    where I: RowIndex,
+          T: FromSql
+{
+    match row.get_opt(index) {
+        Some(val) => {
+            match val {
+                Ok(val) => Some(val),
+                Err(_) => None,
+            }
+        }
+        None => None,
+    }
+}
+
+pub fn get_log(req: &mut Request) -> IronResult<Response> {
+    let query = req.extensions
+        .get::<Router>()
+        .unwrap()
+        .find("entry_id")
+        .unwrap_or("/")
+        .to_string();
+    let query_id = match Uuid::parse_str(&query) {
+        Ok(val) => val,
+        Err(_) => {
+            return Ok(Response::with((status::NotFound, format!("No log {}", query))));
+        }
+    };
+    let conn = get_pg_connection!(&req);
+    let stmt = conn.prepare("SELECT owner, next, prev, data, hlc_tstamp from log where id=$1")
+        .expect("prepare failure");
+    let results = stmt.query(&[&query_id]).expect("bad query");
+    if results.is_empty() {
+        Ok(Response::with((status::NotFound, format!("No log {}", query))))
+    } else {
+        let row = results.get(0);
+        let hlc_tstamp: Vec<u8> = row.get("hlc_tstamp");
+        let when = hybrid_clocks::Timestamp::read_bytes(Cursor::new(hlc_tstamp)).unwrap();
+        let log = Log {
+            id: query_id,
+            owner: row.get("owner"),
+            prev: get_with_null(&row, "prev"),
+            next: get_with_null(&row, "next"),
+            data: row.get("data"),
+            when: when,
+        };
+        Ok(Response::with((status::Ok, serde_json::to_string(&log).unwrap())))
+    }
+}
--- core/src/main.rs
+++ core/src/main.rs
@@ -12,34 +12,25 @@ extern crate iron;
 extern crate router;
 extern crate logger;
 
-use iron::modifiers::Redirect;
 use iron::prelude::*;
-use iron::status;
 use logger::Logger;
 use router::Router;
 
 use std::env;
-use std::io::Read;
-use std::ops::Deref;
 
 extern crate hyper;
 extern crate url;
 
 extern crate uuid;
-use uuid::Uuid;
 extern crate serde;
 extern crate serde_json;
-use serde_json::{Map, Value};
 extern crate hybrid_clocks;
-use std::io::Cursor;
 
 extern crate r2d2;
 extern crate r2d2_postgres;
 extern crate persistent;
 use persistent::Read as PRead;
 use persistent::State;
-use postgres::rows::{Row, RowIndex};
-use postgres::types::FromSql;
 
 #[macro_use]
 extern crate potboiler_common;
@@ -47,128 +38,10 @@ use potboiler_common::db;
 use potboiler_common::server_id;
 
 mod serde_types;
-use serde_types::Log;
 mod notifications;
 mod clock;
 mod nodes;
-use std::sync::Arc;
-
-fn log_status<T: Into<String>>(req: &mut Request, stmt: T) -> IronResult<Response> {
-    let conn = get_pg_connection!(&req);
-    let stmt = conn.prepare(&stmt.into()).expect("prepare failure");
-    let mut logs = Map::new();
-    for row in &stmt.query(&[]).expect("last select works") {
-        let id: Uuid = row.get("id");
-        let owner: Uuid = row.get("owner");
-        logs.insert(owner.to_string(), serde_json::to_value(&id.to_string()));
-    }
-    let value = Value::Object(logs);
-    Ok(Response::with((status::Ok, serde_json::ser::to_string(&value).unwrap())))
-}
-
-fn log_lasts(req: &mut Request) -> IronResult<Response> {
-    log_status(req, "SELECT id, owner from log WHERE next is null")
-}
-
-fn log_firsts(req: &mut Request) -> IronResult<Response> {
-    log_status(req, "SELECT id, owner from log WHERE prev is null")
-}
-
-fn new_log(mut req: &mut Request) -> IronResult<Response> {
-    let conn = get_pg_connection!(&req);
-    let body_string = {
-        let mut body = String::new();
-        req.body.read_to_string(&mut body).expect("could read from body");
-        body
-    };
-    let json: Value = match serde_json::de::from_str(&body_string) {
-        Ok(val) => val,
-        Err(err) => return Err(IronError::new(err, (status::BadRequest, "Bad JSON"))),
-    };
-    let id = Uuid::new_v4();
-    let hyphenated = id.hyphenated().to_string();
-    let server_id = get_server_id!(&req).deref().clone();
-    let stmt = conn.prepare("SELECT id from log WHERE next is null and owner = $1 LIMIT 1")
-        .expect("prepare failure");
-    let results = stmt.query(&[&server_id]).expect("last select works");
-    let previous = if results.is_empty() {
-        None
-    } else {
-        let row = results.get(0);
-        let id: Uuid = row.get("id");
-        Some(id)
-    };
-    let when = clock::get_timestamp(&mut req);
-    let log = Log {
-        id: id,
-        owner: server_id.clone(),
-        prev: previous,
-        next: None,
-        when: when,
-        data: json.clone(),
-    };
-    nodes::insert_log(&conn, &log);
-    let log_arc = Arc::new(log);
-    notifications::notify_everyone(req, log_arc.clone());
-    nodes::notify_everyone(req, log_arc.clone());
-    let new_url = {
-        let req_url = req.url.clone();
-        let base_url = req_url.into_generic_url();
-        base_url.join(&format!("/log/{}", &hyphenated)).expect("join url works")
-    };
-    Ok(Response::with((status::Created,
-                       Redirect(iron::Url::from_generic_url(new_url).expect("URL parsed ok")))))
-}
-
-fn get_with_null<I, T>(row: &Row, index: I) -> Option<T>
-    where I: RowIndex,
-          T: FromSql
-{
-    match row.get_opt(index) {
-        Some(val) => {
-            match val {
-                Ok(val) => Some(val),
-                Err(_) => None,
-            }
-        }
-        None => None,
-    }
-}
-
-fn get_log(req: &mut Request) -> IronResult<Response> {
-    let query = req.extensions
-        .get::<Router>()
-        .unwrap()
-        .find("entry_id")
-        .unwrap_or("/")
-        .to_string();
-    let query_id = match Uuid::parse_str(&query) {
-        Ok(val) => val,
-        Err(_) => {
-            return Ok(Response::with((status::NotFound, format!("No log {}", query))));
-        }
-    };
-    let conn = get_pg_connection!(&req);
-    let stmt = conn.prepare("SELECT owner, next, prev, data, hlc_tstamp from log where id=$1")
-        .expect("prepare failure");
-    let results = stmt.query(&[&query_id]).expect("bad query");
-    if results.is_empty() {
-        Ok(Response::with((status::NotFound, format!("No log {}", query))))
-    } else {
-        let row = results.get(0);
-        let hlc_tstamp: Vec<u8> = row.get("hlc_tstamp");
-        let when = hybrid_clocks::Timestamp::read_bytes(Cursor::new(hlc_tstamp)).unwrap();
-        let log = Log {
-            id: query_id,
-            owner: row.get("owner"),
-            prev: get_with_null(&row, "prev"),
-            next: get_with_null(&row, "next"),
-            data: row.get("data"),
-            when: when,
-        };
-        Ok(Response::with((status::Ok, serde_json::to_string(&log).unwrap())))
-    }
-}
+mod logs;
 
 fn main() {
     log4rs::init_file("log.yaml", Default::default()).unwrap();
@@ -178,10 +51,10 @@ fn main() {
     schema::up(&conn).unwrap();
     let (logger_before, logger_after) = Logger::new(None);
     let mut router = Router::new();
-    router.get("/log", log_lasts);
-    router.post("/log", new_log);
-    router.get("/log/first", log_firsts);
-    router.get("/log/:entry_id", get_log);
+    router.get("/log", logs::log_lasts);
+    router.post("/log", logs::new_log);
+    router.get("/log/first", logs::log_firsts);
+    router.get("/log/:entry_id", logs::get_log);
     router.post("/log/register", notifications::log_register);
     router.post("/log/deregister", notifications::log_deregister);
     router.get("/nodes", nodes::node_list);
