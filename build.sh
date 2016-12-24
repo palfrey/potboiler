d91b3da691163a5db34af7509b25033c75183073
Tom Parker <palfrey@tevp.net>
2016-12-24 00:05:42 +0000

    Push Potboiler images after build

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ if [ "$PROJECT" = "docker" ]; then
 	pip install PyYAML
 	python generate-compose.py 1 > docker-compose.yml
 	docker-compose build
+	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+	docker push potboiler/core
+	docker push potboiler/kv
+	docker push potboiler/pigtail
 	exit 0
 fi
 
