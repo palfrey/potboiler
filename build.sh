7f87b4b4533c8ccd59f2ecb457cdcc7b2519da04
Tom Parker-Shemilt <palfrey@tevp.net>
2018-12-09 17:54:03 +0000

    Actually run the system in build

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ if [ "$PROJECT" = "docker" ]; then
 	pip install PyYAML
 	python generate-compose.py 1 > docker-compose.yml
 	docker-compose build
+	docker-compose up &
+	./wait-for-port.sh localhost 8001
+	sleep 5 # to wait for fully booted	
+	curl -v http://localhost:8001/kv/_config
 	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 	docker push potboiler/core
 	docker push potboiler/kv
