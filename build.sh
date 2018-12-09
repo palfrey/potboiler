2f6693a9cc591972989e5160773bb9e6fa3c3c99
Tom Parker-Shemilt <palfrey@tevp.net>
2018-12-09 23:12:36 +0000

    Split out check project

--- build.sh
+++ build.sh
@@ -14,11 +14,14 @@ if [ "$PROJECT" = "docker" ]; then
 	docker push potboiler/kv
 	docker push potboiler/pigtail
 	exit 0
+elif [ "$PROJECT" = "check" ]; then
+	rustup component add rustfmt-preview
+	rustup component add clippy-preview
+	cargo fmt -- --check
+	cargo clippy
 fi
 
 export PATH=$PATH:~/.cargo/bin &&
-cd $PROJECT
-cargo fmt -- --check
-cargo clippy
+cd "$PROJECT"
 cargo build
 cargo test
